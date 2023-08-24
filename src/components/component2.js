6ee280ef93293132fd0923049e5ce570af15984a
piyush <pmimpiyush@gmail.com>
2023-08-25 01:32:42 +0530

    commit

--- src/components/component2.js
+++ src/components/component2.js
@@ -15,13 +15,19 @@ export default function Component2(props) {
  
   return (
     <div className='component2'>
+      <div className='component2-text'>
+      <h1 className='about-airsoc-heading'> About Airsoc </h1>
+      <p className='about-airsoc-text'>AirSoc IIT Roorkee is the official campus group that actively works on Aircraft, Drone, and Rocket Design. We are a bunch of enthusiastic geeks who are passionate about developments in the aircraft industry and continuously working in the same to thrive for excellence. Our aim is to inculcate the culture of airborne vehicles among IIT Roorkee junta and bring glory to IITR through national and international competitions.</p>
+    
+      </div>
+   
     <div className='animation'>
       <img onClick={props.animate} className={`plane-a ${props.a1}`} src={animationimg1} alt='.'></img>
       <img onClick={props.animate} className={`plane-b ${props.a31}`} src={animationimg2} alt='.'></img>
       <img onClick={props.animate} className={`plane-c ${props.a32}`} src={animationimg3} alt='.'></img>
       <img onClick={props.animate} className={`drone ${props.a21}`} src={animationimg4} alt='.'></img>
       <img  onClick={props.animate} className={`drone ${props.a22}`} src={animationimg5} alt='.'></img>
-
+      
     </div></div>
   )
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -23,6 +23,19 @@ export default function Navbar() {
         <a className='link' href='/'>Contact</a>
         </div></div>
     </div>
-<img className='big-img' src={img3} alt='.'></img></div>
+    <div className='test'>
+    <svg className='svg'  viewBox="0 0 1440 520"> 
+    <defs>
+    <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+      <stop offset="0%" stop-color="#605195" />
+      <stop offset="100%" stop-color="#E07785" />
+    </linearGradient>
+  </defs>
+   <path  fill="url(#gradient)" fill-opacity="1" d="M0,224L40,224C80,224,160,224,240,208C320,192,400,160,480,165.3C560,171,640,213,720,213.3C800,213,880,171,960,149.3C1040,128,1120,
+    128,1200,154.7C1280,181,1360,235,1400,261.3L1440,288L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
+    </svg>
+    </div>
+    
+</div>
   )
 }
