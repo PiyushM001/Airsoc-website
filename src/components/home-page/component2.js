eafc01a4e45f64b8b909397c51d7ad1e0a72368b
piyush <pmimpiyush@gmail.com>
2023-09-21 04:20:10 +0530

    responsive navebar and component 2

--- src/components/home-page/component2.js
+++ src/components/home-page/component2.js
@@ -1,29 +1,45 @@
-import React  from 'react' ;
-import './component2.css'
-import animationimg1 from  '../../images/plane_a.png';
-import animationimg2 from '../../images/plane_b.png';
-import animationimg3 from '../../images/plane_c.png';
-import animationimg4 from '../../images/drone.png';
-import animationimg5 from '../../images/rocket.png';
-
-
-
-export default function Component2(props) {
+import React from "react";
+import img from "../../images/PP1P.svg";
+import img2 from "../../images/Airsoc 2.svg";
+import "./component2.css";
+export default function Component2() {
   return (
-    <div className='component2'>
-      <div className='component2-text'>
-      <h1 className='about-airsoc-heading'> About Airsoc </h1>
-      <p className='about-airsoc-text'>AirSoc IIT Roorkee is the official campus group that actively works on Aircraft, Drone, and Rocket Design. We are a bunch of enthusiastic geeks who are passionate about developments in the aircraft industry and continuously working in the same to thrive for excellence. Our aim is to inculcate the culture of airborne vehicles among IIT Roorkee junta and bring glory to IITR through national and international competitions.</p>
-    
+    <div>
+      <div className="plane-img-div">
+        <img className="plane-img" src={img}></img>
       </div>
-   
-    <div className='animation'>
-      <img onClick={props.animate} className={`plane-a ${props.a1}`} src={animationimg1} alt='.'></img>
-      <img onClick={props.animate} className={`plane-b ${props.a31}`} src={animationimg2} alt='.'></img>
-      <img onClick={props.animate} className={`plane-c ${props.a32}`} src={animationimg3} alt='.'></img>
-      <img onClick={props.animate} className={`drone ${props.a21}`} src={animationimg4} alt='.'></img>
-      <img  onClick={props.animate} className={`drone ${props.a22}`} src={animationimg5} alt='.'></img>
-      
-    </div></div>
-  )
+
+      <div className="component2-div">
+        <svg className="svg-component2" viewBox="0 130 1440 180">
+          <defs>
+            <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+              <stop offset="0%" stop-color="#4a349b" />
+              <stop offset="100%" stop-color="#e07785" />
+            </linearGradient>
+          </defs>
+          <path
+            fill="url(#gradient)"
+            fill-opacity="1"
+            d="M0,224L40,224C80,224,160,224,240,208C320,192,400,160,480,165.3C560,171,640,213,720,213.3C800,213,880,171,960,149.3C1040,128,1120,
+    128,1200,154.7C1280,181,1360,235,1400,261.3L1440,288L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+          ></path>
+        </svg>
+
+        <img className="airsoc-logo-2" src={img2}></img>
+        <h1 className="airsoc-logo-text">AirSoc</h1>
+        <h2 className="airsoc-logo-text2">IIT Roorkee</h2>
+        <h1 className="airsoc-logo-text3">About AirSoc</h1>
+        <div className="component2-text-div">
+        <p >
+          AirSoc IIT Roorkee is the official campus group that actively works on
+          Aircraft, Drone, and Rocket Design. We are a bunch of enthusiastic
+          geeks who are passionate about developments in the aircraft industry
+          and continuously working in the same to thrive for excellence. Our aim
+          is to inculcate the culture of airborne vehicles among IIT Roorkee
+          junta and bring glory to IITR through national and international
+          competitions.
+        </p></div>
+      </div>
+    </div>
+  );
 }
--- src/components/home-page/navbar.js
+++ src/components/home-page/navbar.js
@@ -1,41 +1,85 @@
-import React from 'react'
-import './navbar.css'
-import img from '../../images/iitr-logo.svg'
-import img2 from '../../images/Airsoc 2.svg'
+import React, { useState } from "react";
+import "./navbar.css";
+import img from "../../images/iitr-logo.svg";
+import img2 from "../../images/Airsoc 2.svg";
 
 export default function Navbar() {
+  const [list, setlist] = useState("list-div");
+  const navfunction = () => {
+    setlist("list-div list-div-");
+  };
+  const crossfunction = () => {
+    setlist("list-div");
+  };
+
   return (
     <div>
-    <div className="nav">
-        <img id='logo' className='logo-margin' src={img} alt="."></img>
-        <img className='logo-margin' src={img2} alt="."></img>
-        <div className='airsoc-logo-div'>
-        <h1 className='airsoc'>AirSoc</h1> 
-        <h3 className='iitr'> IIT Roorkee</h3>
+      <div className="nav">
+        <img id="logo" className="logo-margin" src={img} alt="."></img>
+        <img id="airsoc-logo" className="logo-margin" src={img2} alt="."></img>
+        <div className="airsoc-logo-div">
+          <h1 className="airsoc">AirSoc</h1>
+          <h3 className="iitr"> IIT Roorkee</h3>
         </div>
-        <div className='links0'>
-        <div className='links'>
-        <a className='link-home link' href='/'>Home </a>
-        <a className='link' href='/'>Team </a>
-        <a className='link' href='/'>Project</a>
-        <a className='link' href='/'>Milestone</a>
-        <a className='link' href='/'>Gallery </a>
-        <a className='link' href='/'>Contact</a>
-        </div></div>
+        <div className="links0">
+          <div className="links">
+            <a className="link" href="/">
+              Home{" "}
+            </a>
+            <a className="link" href="/">
+              Team{" "}
+            </a>
+            <a className="link" href="/">
+              Project
+            </a>
+            <a className="link" href="/">
+              Milestone
+            </a>
+            <a className="link" href="/">
+              Gallery{" "}
+            </a>
+            <a className="link" href="/">
+              Contact
+            </a>
+          </div>
+        </div>
+        <div className="hamburger-div">
+          <div onClick={navfunction}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        <div className={`${list}`}>
+          <div className="list">
+            <div className="list-">
+              <div onClick={crossfunction} className="cross-div">
+                <span className="cross-span-1"></span>
+                <span className="cross-span-2"></span>
+              </div>
+              <a className="list-anchor-tags" href="/">
+                Home
+              </a>
+              <a className="list-anchor-tags" href="/">
+                Team
+              </a>
+              <a className="list-anchor-tags" href="/">
+                Project
+              </a>
+              <a className="list-anchor-tags" href="/">
+                Milestone
+              </a>
+              <a className="list-anchor-tags" href="/">
+                Gallery
+              </a>
+              <a className="list-anchor-tags" href="/">
+                Contact
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div className='test'>
-    <svg className='svg'  viewBox="0 0 1440 520"> 
-    <defs>
-    <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-      <stop offset="0%" stop-color="#605195" />
-      <stop offset="100%" stop-color="#E07785" />
-    </linearGradient>
-  </defs>
-   <path  fill="url(#gradient)" fill-opacity="1" d="M0,224L40,224C80,224,160,224,240,208C320,192,400,160,480,165.3C560,171,640,213,720,213.3C800,213,880,171,960,149.3C1040,128,1120,
-    128,1200,154.7C1280,181,1360,235,1400,261.3L1440,288L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
-    </svg>
-    </div>
-    
-</div>
-  )
+  );
 }
